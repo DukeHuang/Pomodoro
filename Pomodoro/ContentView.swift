29685ac01a4f40a5daba6bafa41b86af175e048f
yongyou <>
2020-04-29 10:34:53 +0800

    init commit

---
+++ Pomodoro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by yongyou on 2020/4/29.
+//  Copyright © 2020 yongyou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
